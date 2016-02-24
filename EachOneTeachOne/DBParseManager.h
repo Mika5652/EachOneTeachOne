e113357fc55a28d4bdd1dedb0c84417dad5e2e30
VojtekC <vczepiec.gmail.com>
2016-02-24 15:57:43 +0100

    Parse manager

---
+++ EachOneTeachOne/DBParseManager.h
@@ -0,0 +1,13 @@
+//
+//  DBParseManager.h
+//  EachOneTeachOne
+//
+//  Created by Vojtěch Czepiec on 24/02/16.
+//  Copyright © 2016 Michael Pohl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DBParseManager : NSObject
+
+@end
---
+++ EachOneTeachOne/DBParseManager.m
@@ -0,0 +1,13 @@
+//
+//  DBParseManager.m
+//  EachOneTeachOne
+//
+//  Created by Vojtěch Czepiec on 24/02/16.
+//  Copyright © 2016 Michael Pohl. All rights reserved.
+//
+
+#import "DBParseManager.h"
+
+@implementation DBParseManager
+
+@end
